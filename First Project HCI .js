383ae85aebabfa6b86bc76798459867965e9280b
Dylan Havis <64048857+Dylpickle08@users.noreply.github.com>
2021-10-04 15:24:50 -0400

    Add files via upload

---
+++ First Project HCI .js
@@ -0,0 +1,68 @@
+
+
+	
+let courses = [
+    {"Line":81,"Department":"BUS","Number":344,"Section":1,"Title":"MANAGEMENT OF INFORMATION SYSTEMS","Faculty":"Richards, Gordon P.","Openings":2,"Capacity":30,"Status":"Open","Day":"MWF","Time":"1:25-2:20 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 341 Computer Science Lab","Credits":3,"Start":"8\/30\/2021","End":"12\/17\/2021\r\n"}
+    ,{"Line":167,"Department":"CSC","Number":133,"Section":2,"Title":"SURVEY OF COMPUTER SCIENCE","Faculty":"Madeira, Scott","Openings":6,"Capacity":15,"Status":"Open","Day":"H","Time":"2:00-4:50 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 341 Computer Science Lab","Credits":0,"Start":"8\/30\/2021","End":"12\/17\/2021\r\n"}
+    ,{"Line":168,"Department":"CSC","Number":133,"Section":3,"Title":"SURVEY OF COMPUTER SCIENCE","Faculty":"Madeira, Scott","Openings":7,"Capacity":15,"Status":"Open","Day":"T","Time":"6:30-9:20 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 341 Computer Science Lab","Credits":0,"Start":"8\/30\/2021","End":"12\/17\/2021\r\n"}
+    ,{"Line":169,"Department":"CSC","Number":133,"Section":"0A","Title":"SURVEY OF COMPUTER SCIENCE","Faculty":"Richards, Gordon P.","Openings":15,"Capacity":45,"Status":"Open","Day":"TH","Time":"8:00-9:20 AM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 110 Chemistry room","Credits":4,"Start":"8\/30\/2021","End":"12\/17\/2021\r\n"}
+    ,{"Line":170,"Department":"CSC","Number":190,"Section":1,"Title":"HTML","Faculty":"Madeira, Scott","Openings":4,"Capacity":25,"Status":"Open","Day":"M","Time":"2:30-3:25 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 312A","Credits":1,"Start":"8\/30\/2021","End":"12\/17\/2021\r\n"}
+    ,{"Line":171,"Department":"CSC","Number":205,"Section":1,"Title":"HCI DESIGN & PROGRAMMING","Faculty":"Madeira, Scott","Openings":10,"Capacity":25,"Status":"Open","Day":"MWF","Time":"11:15-12:10 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 341 Computer Science Lab","Credits":3,"Start":"8\/30\/2021","End":"12\/17\/2021\r\n"}
+    ,{"Line":172,"Department":"CSC","Number":344,"Section":1,"Title":"MANAGEMENT INFORMATION SYSTEM","Faculty":"Poteete, Paul W. Steffine, Aaron","Openings":2,"Capacity":90,"Status":"Open","Day":"MWF","Time":"1:25-2:20 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 341 Computer Science Lab","Credits":3,"Start":"8\/30\/2021","End":"12\/17\/2021\r\n"}
+    ,{"Line":173,"Department":"CSC","Number":363,"Section":"E1","Title":"DATABASE SYSTEMS","Faculty":"Hinderliter, Jeffery A","Openings":4,"Capacity":30,"Status":"Open","Day":"T","Time":"6:30-9:20 PM","Campus":" Main Campus","Building":" Science and Engineering","Room":" SE 233 Engineering Lab\/Classroom","Credits":3,"Start":"8\/30\/2021","End":"12\/17\/2021\r\n"}
+    ,{"Line":296,"Department":"HUM","Number":103,"Section":"0A","Title":"INVITATION TO THE HUMANTIES","Faculty":"Miller, Eric John","Openings":12,"Capacity":180,"Status":"Open","Day":"W","Time":"11:15-12:10 PM","Campus":" Main Campus","Building":" Old Main","Room":" John White Chapel","Credits":0,"Start":"8\/30\/2021","End":"12\/17\/2021"}
+]
+]
+Function generateTableHead(table,data) {
+	let thead = table.createTHead();
+	let row = thead.insertRow();
+	for (let key of data) {
+	let th = document.createElement("th");
+	let text = document.createTextNode(key);
+	th.appendChild(text);
+	row.appendChild(th);
+	}
+}
+function generateTable(table,data) {
+	for (let element of data) {
+		let row = table.insertRow();
+		for (key in element) {
+			let cell = row.insertCell();
+			let text = document.createTextNode(element[key]);
+			cell.appendChild(text);
+		}
+	}
+}
+let table = document.querySelector("table");
+let data = Object.keys(classes[0]);
+generateTable(table, classes);
+generateTableHead(table,data);
+
+
+function cutData(classes)
+{
+	courses.forEach(function (course) {
+        delete course.Section;
+		delete course.Openings;
+		delete course.Capacity;
+		delete course.StartTime;
+		delete course.EndTime;
+		delete course.Campus; 
+		delete course.Building;
+		delete course.Line;
+		delete course.StartDate;
+		delete course.EndDate;
+    });
+    return classes;
+}
+
+
+
+
+
+
+
+
+
+
+
